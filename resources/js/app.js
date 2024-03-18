cfd36f6540a0e7d7777d02304032927289d60fc8
Erik Tailor <erno22szabo@gmail.com>
2024-03-18 05:38:03 +0100

    save

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,19 +1,34 @@
 /**
- * PROJECT PREPARATION
+ * ------------------------------------------------------------------------------------
+ * PROJECT SETUP
  * ------------------------------------------------------------------------------------
  */
 
-// prevent dropzone from autodiscover
-Dropzone.autoDiscover = false;
+// import jQuery
+import './bootstrap.js';
 
-// ajax csrf token setup
+// import jQuery UI
+import 'jquery-ui/dist/jquery-ui';
+
+// import Dropzone
+import { Dropzone } from 'dropzone';
+
+// ajax csrf setup
 $.ajaxSetup({
     headers: {
         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
     }
 });
 
+// prevent dropzone autodiscover
+Dropzone.autoDiscover = false;
+
+// set global variables
+var errorMessage = $('#dzErrorMessage'),
+    placeHolder = $('#dzPlaceholder');
+
 /**
+ * ------------------------------------------------------------------------------------
  * DROPZONE SETUP
  * ------------------------------------------------------------------------------------
  */
@@ -35,13 +50,160 @@ var myDropzone = new Dropzone('#dzDropzone', {
 });
 
 /**
- * Send csrf token with dropzone
+ * If files dragged into dropzone
+ */
+myDropzone.on('addedfile', function(file) {
+    errorMessage.hide();
+    placeHolder.hide();
+    
+    // Generate a temporary identifier for each file
+    file.tempId = 'temp_' + file.name + '_' + file.size + '_' + file.lastModified;
+    file.previewElement.setAttribute('data-id', file.tempId);
+    
+    setTimeout(() => {
+
+        // Update layout for existing previews
+        const previews = $(myDropzone.previewsContainer).children(':not(.dz-additional-area)');
+        previews.addClass('col-md-6');
+        if (myDropzone.files.length >= 1) {
+            previews.eq(0).removeClass('col-md-6').addClass('col-12');
+            $('#dzDropzone').addClass('border-0');
+        }
+
+        // Adds the cover badge to first image
+        var coverBadge = $('#dzBadgeTemplate').html();
+        if (previews.eq(0).find('.dz-cover-badge').length === 0) {
+            previews.eq(0).find('.dz-image').append(coverBadge);
+        }
+        
+        updateAdditionalAreas(); 
+    }, 0);
+});
+
+/**
+ * If dropzone has validation errors
+ */
+myDropzone.on('error', function(file, response) {
+    errorMessage.show().text(response);
+    this.removeFile(file);
+
+    // if there are no more files in Dropzone, show the placeholder
+    if (myDropzone.files.length < 1) {
+        placeHolder.show();
+    }
+});
+
+/**
+ * On sending event (attach csrf token)
  */
 myDropzone.on('sending', function(file, xhr, formData) {
     formData.append('_token', $('meta[name="csrf-token"]').attr('content'));
 });
 
 /**
+ * Adjust additional upload areas
+ */
+function updateAdditionalAreas() {
+    let additionalTemplate = $('#dzAdditionalTemplate').html();
+    let filesCount = myDropzone.files.length;
+    let additionalAreas = 0;
+
+    // remove all additional areas first
+    $(myDropzone.previewsContainer).find('.dz-additional-area').parent().remove();
+
+    additionalAreas = 9 - filesCount;
+
+    for (let i = 0; i < additionalAreas; i++) {
+        $(myDropzone.previewsContainer).append(additionalTemplate);
+    }
+}
+
+/**
+ * If an additional area is clicked
+ */
+$(document).on('click', '.dz-additional-area', function() {
+    if (myDropzone.hiddenFileInput) {
+
+        // open the file browser
+        myDropzone.hiddenFileInput.click();
+    }
+});
+
+/**
+ * Remove image function
+ */
+$(document).on('click', '.dz-remove-button', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    var filePreview = $(this).closest('.dz-image-preview');
+    var fileId = filePreview.data('file-id');
+
+    // find the corresponding Dropzone object
+    var fileToRemove = myDropzone.files.find(function(file) {
+        return file.uniqueId === fileId;
+    });
+
+    if (fileToRemove) {
+
+        // Capture the index of the file to be removed before actually removing it
+        let wasCoverImage = filePreview.index() === 0;
+
+        // remove the file
+        myDropzone.removeFile(fileToRemove);
+
+        // use setTimeout to delay the execution of the layout adjustment
+        // this gives Dropzone enough time to update its internal state
+        setTimeout(() => {
+            if (wasCoverImage && myDropzone.files.length > 0) {
+
+                // the removed file was the cover image and there are still other files left
+                setTimeout(() => {
+                    const previews = $(myDropzone.previewsContainer).children(':not(.dz-additional-area)');
+                    previews.removeClass('col-12 col-md-6').addClass('col-md-6');
+
+                    if (previews.length > 0) {
+
+                        // make the first preview the new cover
+                        previews.eq(0).removeClass('col-md-6').addClass('col-12');
+                        $('#dzDropzone').addClass('border-0');
+
+                        // ensure the cover badge is correctly placed
+                        $('.dz-cover-badge').remove();
+                        previews.eq(0).find('.dz-image').append($('#dzBadgeTemplate').html());
+
+                    } else {
+
+                        // if there are no more previews, show the message again
+                        placeHolder.show();
+                        $('#dzDropzone').removeClass('border-0');
+                    }
+
+                    // Update the additional areas in case the count needs adjusting
+                    updateAdditionalAreas(); 
+                }, 0);
+
+            } else if (myDropzone.files.length === 0) {
+
+                // if there are no more files, possibly show the upload prompt again
+                placeHolder.show();
+                $('#dzDropzone').removeClass('border-0');
+                $('.dz-additional-area').parent().remove();
+
+            } else {
+
+                // update the additional areas in case the count needs adjusting
+                updateAdditionalAreas(); 
+            }
+        }, 10);
+    }
+
+    // hide error messages
+    errorMessage.hide();
+});
+
+/**
+ * ------------------------------------------------------------------------------------
  * SORTABLE SETUP
  * ------------------------------------------------------------------------------------
  */
@@ -71,9 +233,11 @@ $('#dzPreviews').sortable({
     }
 });
 
-
-
-
+/**
+ * ------------------------------------------------------------------------------------
+ * FINAL STEPS
+ * ------------------------------------------------------------------------------------
+ */
 
 /**
  * Submit images upload
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,8 @@
 <head>
     <title>Laravel Dropzone Sortable</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    @vite(['resources/sass/app.scss'])
+    
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
 
@@ -22,7 +22,7 @@
             <div class="position-relative">
                 <div class="form-group mb-3">
                     <div class="main-drag-area form-control p-0" id="dzDropzone">
-                        <div class="dz-message text-muted opacity-75" id="dzPlaceholder">
+                        <div class="dz-message rounded-3 text-muted opacity-75" id="dzPlaceholder">
                             <svg class="mb-3" width="50px" height="50px" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style="display: block; height: 64px; width: 64px; fill: currentcolor;"><path d="M41.636 8.404l1.017 7.237 17.579 4.71a5 5 0 0 1 3.587 5.914l-.051.21-6.73 25.114A5.002 5.002 0 0 1 53 55.233V56a5 5 0 0 1-4.783 4.995L48 61H16a5 5 0 0 1-4.995-4.783L11 56V44.013l-1.69.239a5 5 0 0 1-5.612-4.042l-.034-.214L.045 14.25a5 5 0 0 1 4.041-5.612l.215-.035 31.688-4.454a5 5 0 0 1 5.647 4.256zm-20.49 39.373l-.14.131L13 55.914V56a3 3 0 0 0 2.824 2.995L16 59h21.42L25.149 47.812a3 3 0 0 0-4.004-.035zm16.501-9.903l-.139.136-9.417 9.778L40.387 59H48a3 3 0 0 0 2.995-2.824L51 56v-9.561l-9.3-8.556a3 3 0 0 0-4.053-.009zM53 34.614V53.19a3.003 3.003 0 0 0 2.054-1.944l.052-.174 2.475-9.235L53 34.614zM48 27H31.991c-.283.031-.571.032-.862 0H16a3 3 0 0 0-2.995 2.824L13 30v23.084l6.592-6.59a5 5 0 0 1 6.722-.318l.182.159.117.105 9.455-9.817a5 5 0 0 1 6.802-.374l.184.162L51 43.721V30a3 3 0 0 0-2.824-2.995L48 27zm-37 5.548l-5.363 7.118.007.052a3 3 0 0 0 3.388 2.553L11 41.994v-9.446zM25.18 15.954l-.05.169-2.38 8.876h5.336a4 4 0 1 1 6.955 0L48 25.001a5 5 0 0 1 4.995 4.783L53 30v.88l5.284 8.331 3.552-13.253a3 3 0 0 0-1.953-3.624l-.169-.05L28.804 14a3 3 0 0 0-3.623 1.953zM21 31a4 4 0 1 1 0 8 4 4 0 0 1 0-8zm0 2a2 2 0 1 0 0 4 2 2 0 0 0 0-4zM36.443 6.11l-.175.019-31.69 4.453a3 3 0 0 0-2.572 3.214l.02.175 3.217 22.894 5.833-7.74a5.002 5.002 0 0 1 4.707-4.12L16 25h4.68l2.519-9.395a5 5 0 0 1 5.913-3.587l.21.051 11.232 3.01-.898-6.397a3 3 0 0 0-3.213-2.573zm-6.811 16.395a2 2 0 0 0 1.64 2.496h.593a2 2 0 1 0-2.233-2.496zM10 13a4 4 0 1 1 0 8 4 4 0 0 1 0-8zm0 2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z"></path></svg>
                             <span>Drag your images here to upload</span>
                         </div>
@@ -31,7 +31,7 @@
                 </div>
             </div>
             <div class="invalid-feedback mb-3" id="dzErrorMessage"></div>
-            <button class="btn btn-lg btn-primary" id="dzSubmitButton">Upload Images</button>
+            <button class="btn btn-lg btn-primary fs-6 py-3 px-4" id="dzSubmitButton">Upload Images</button>
         </form>
 
     </div>
@@ -55,12 +55,6 @@
     <script id="dzBadgeTemplate" type="text/template">
         <span class="dz-cover-badge badge bg-black bg-opacity-75 position-absolute top-0 start-0 m-2 p-2">Cover Image</span>
     </script>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.7.0/dropzone.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-
-    @vite(['resources/js/app.js'])
 
 </body>
 </html>
